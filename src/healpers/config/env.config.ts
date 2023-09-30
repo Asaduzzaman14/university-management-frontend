5c5164906268c9a38d073a44212421670dac4569
Md Asaduzaman <asadasad9282@gmail.com>
2023-09-30 22:54:38 +0600

    Create baseApi endpoints for authentication using RTK query

---
+++ src/healpers/config/env.config.ts
@@ -0,0 +1,3 @@
+export const getbaseUrl = (): string => {
+  return process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3030";
+};
---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,11 @@
+import { axiosBaseQuery } from "@/healpers/axios/axiosBaseQuery ";
+import { getbaseUrl } from "@/healpers/config/env.config";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const baseAPi = createApi({
+  reducerPath: "api",
+  baseQuery: axiosBaseQuery({ baseUrl: getbaseUrl() }),
+  endpoints: () => ({}),
+  tagTypes: ["user"],
+});
