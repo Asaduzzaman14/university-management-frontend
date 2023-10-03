e445db6f001ef52abd21c01d4aacede39898aecf
Md Asaduzaman <asadasad9282@gmail.com>
2023-10-03 14:49:36 +0600

    Create reusable Stepper Form to create student form

--- src/app/(withLayout)/super_admin/manage-students/create/page.tsx
+++ src/app/(withLayout)/super_admin/manage-students/create/page.tsx
@@ -1,9 +1,31 @@
+import StepperForm from "@/components/StepperForm/StepperForm";
+import StudentInfo from "@/components/StudentForms/StudentInfo";
 import React from "react";
 
 const CreateStudent = () => {
+  const steps = [
+    {
+      title: "Student Information",
+      content: <StudentInfo></StudentInfo>,
+    },
+    {
+      title: "Basic Information",
+      content: <StudentInfo></StudentInfo>,
+    },
+    {
+      title: "Guardian Information",
+      content: <StudentInfo></StudentInfo>,
+    },
+    {
+      title: "Local Guardian Information",
+      content: <StudentInfo></StudentInfo>,
+    },
+  ];
+
   return (
     <div>
       <h2>Create Student</h2>
+      <StepperForm steps={steps}></StepperForm>
     </div>
   );
 };
---
+++ src/components/StudentForms/StudentInfo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Student info</h1>
+    </div>
+  );
+};
+
+export default StudentInfo;
