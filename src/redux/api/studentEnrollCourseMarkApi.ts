558ee194ac6abd3cc77da907a7e1e24863dc32a4
Md Asaduzaman <asadasad9282@gmail.com>
2023-10-22 21:28:02 +0600

    update final marks

---
+++ src/redux/api/studentEnrollCourseMarkApi.ts
@@ -0,0 +1,73 @@
+import { tagTypes } from "../tag-types";
+import { IMeta, IStudentEnrolledCourseMark } from "../types";
+import { baseAPi } from "./baseApi";
+
+export const BASE_STUDENT_COURSE_MARKS = "/student-enrolled-course-marks";
+
+const studentEnrollCourseMarkApi = baseAPi.injectEndpoints({
+  endpoints: (build) => ({
+    myMarks: build.query({
+      query: (arg: Record<string, any>) => {
+        return {
+          url: `${BASE_STUDENT_COURSE_MARKS}/my-marks`,
+          method: "GET",
+          params: arg,
+        };
+      },
+      transformResponse: (
+        response: IStudentEnrolledCourseMark[],
+        meta: IMeta
+      ) => {
+        return {
+          myMarks: response,
+          meta,
+        };
+      },
+      providesTags: [tagTypes.student],
+    }),
+    studentEnrolledCourseMarks: build.query({
+      query: (arg: Record<string, any>) => {
+        return {
+          url: `${BASE_STUDENT_COURSE_MARKS}`,
+          method: "GET",
+          params: arg,
+        };
+      },
+      transformResponse: (
+        response: IStudentEnrolledCourseMark[],
+        meta: IMeta
+      ) => {
+        return {
+          studentEnrolledCourseMarks: response,
+          meta,
+        };
+      },
+      providesTags: [tagTypes.student],
+    }),
+    updateMarks: build.mutation({
+      query: (data) => ({
+        url: `${BASE_STUDENT_COURSE_MARKS}/update-marks`,
+        method: "POST",
+        data,
+      }),
+      invalidatesTags: [tagTypes.student],
+    }),
+    updateFinalMarks: build.mutation({
+      query: (data) => ({
+        url: `${BASE_STUDENT_COURSE_MARKS}/update-course-final-marks`,
+        method: "POST",
+        data,
+      }),
+      invalidatesTags: [tagTypes.student],
+    }),
+  }),
+});
+
+export const {
+  useUpdateMarksMutation,
+  useMyMarksQuery,
+  useStudentEnrolledCourseMarksQuery,
+  useUpdateFinalMarksMutation,
+} = studentEnrollCourseMarkApi;
+
+export default studentEnrollCourseMarkApi;
